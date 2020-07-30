e52ee636865f639a5cd6bbc7f4d08262a63ac82e
AlexParshyn <parshin_o@ukr.net>
2020-07-30 16:35:49 +0300

    Added possible panic situation with pointers and methods

--- structures.go
+++ structures.go
@@ -14,7 +14,7 @@ type human struct {
 }
 
 type worker struct {
-	personInfo          human
+	personInfo          *human
 	education           string
 	yearsOfExperience   int
 	onVacation          bool
@@ -25,45 +25,53 @@ type worker struct {
 }
 
 type schoolTeacher struct {
-	basics     worker
+	basics     *worker
 	Classes    []int  `json:"classes,omitempty"`
 	Subject    string `json:"studyingSubject,omitempty"`
 	ClassTrips bool   `json:"trips,omitempty"`
 }
 
 type programmer struct {
-	basics              worker
+	basics              *worker
 	Post                string   `json:"post,omitempty"`
 	Languages           []string `json:"languages,omitempty"`
 	FreelanceExperience bool     `json:"freelance,omitempty"`
 }
 
 type footballer struct {
-	basics           worker
+	basics           *worker
 	PlayPosition     string `json:"position"`
 	League           string `json:"league,omitempty"`
 	MonthsOfContract int    `json:"contract,omitempty"`
 }
 
 type doctor struct {
-	basics          worker
+	basics          *worker
 	License         string `json:"license"`
 	Hospital        string `json:"hospital"`
 	NeededEquipment string `json:"equipment,omitempty"`
 }
 
 type driver struct {
-	basics                worker
+	basics                *worker
 	DrivingCategories     string `json:"categories"`
 	Region                string `json:"region,omitempty"`
 	ForeignCountriesVisas string `json:"visas,omitempty"`
 }
 
+func (hum *human) getContact() {
+	fmt.Printf("Contacts of %s %s: email - %s, phone - %s. \n", hum.name, hum.surname, hum.email, hum.phone)
+}
+
+func (wor *worker) getSalary() {
+	fmt.Printf("%s %s earns %.2f$ per hour and works %d hours per week. \n", wor.personInfo.name, wor.personInfo.surname, wor.salaryPerHour, wor.workingHoursPerWeek)
+}
+
 func main() {
 	var (
 		teacher1 = schoolTeacher{
-			basics: worker{
-				personInfo: human{
+			basics: &worker{
+				personInfo: &human{
 					name:    "Dale",
 					surname: "Green",
 					age:     27,
@@ -84,5 +92,14 @@ func main() {
 			ClassTrips: false,
 		}
 	)
-	fmt.Printf("We declared such people: " + teacher1.basics.personInfo.name + " " + teacher1.basics.personInfo.surname)
+	var p *schoolTeacher = &teacher1
+
+	fmt.Printf("We declared such people: " + p.basics.personInfo.name + " " + p.basics.personInfo.surname + "\n")
+	teacher1.basics.personInfo.getContact()
+	teacher1.basics.getSalary()
+
+	// Как вызвать панику при помощи указателей - обратиться к полям объявленной, но не инициализированной структуры (через указатель)
+	//var pPanic *schoolTeacher
+	//fmt.Printf("We declared such people: " + pPanic.basics.personInfo.name + " " + pPanic.basics.personInfo.surname)
+
 }
